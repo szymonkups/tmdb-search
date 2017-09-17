cdcd6ea4594a8330f5b47cc3c0d77b1a88267196
Szymon Kupś <szymonkups@gmail.com>
2017-09-18 00:05:25 +0200

    Added two stateless components - loading indicatior and button.

---
+++ src/components/LoadMoreButton/LoadMoreButton.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './LoadMoreButton.css';
+
+const LoadMoreButton = props => {
+	return (
+		<div className="row">
+			<div className="load-more col-sm-10 col-sm-push-1 col-md-8 col-md-push-2" >
+				<button className="load-more--button" onClick={ props.onClick }>
+					<i className="fa fa-arrow-down load-more--icon" aria-hidden="true"></i>
+				</button>
+			</div>
+		</div>
+	);
+};
+
+export default LoadMoreButton;
---
+++ src/components/LoadProgress/LoadProgress.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const LoadProgress = () => {
+	return (
+		<div className="row">
+			<div className="load-progress col-sm-10 col-sm-push-1 col-md-8 col-md-push-2" >
+				<span className="load-progress--spinner">
+					<i className="fa fa-cog fa-spin fa-3x fa-fw" aria-hidden="true"></i><br />
+					Loading data...
+				</span>
+			</div>
+		</div>
+	);
+};
+
+export default LoadProgress;
