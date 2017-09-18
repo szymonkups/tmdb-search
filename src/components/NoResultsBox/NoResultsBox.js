6913c92fd78c99ad3ca730411dc3b280b677867b
Szymon Kupś <szymonkups@gmail.com>
2017-09-18 16:17:05 +0200

    Refactored NoResultsBox component to stateless one.

--- src/components/NoResultsBox/NoResultsBox.js
+++ src/components/NoResultsBox/NoResultsBox.js
@@ -1,18 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './NoResultsBox.css';
 
 /**
  * Information displayed when there are no search results to display.
  */
-class NoResultsBox extends Component {
-	render() {
-		return (
-			<div className="no-results-box">
-				<h1>No results found.</h1>
-				<h2>Use search box above.</h2>
-			</div>
-		);
-	}
-}
+const NoResultsBox = () => (
+	<div className="no-results-box">
+		<h1>No results found.</h1>
+		<h2>Use search box above.</h2>
+	</div>
+);
 
 export default NoResultsBox;
