78e504921abf4e3866dffaea8f01630cef87ed74
Szymon Kupś <szymonkups@gmail.com>
2017-09-18 16:31:22 +0200

    Simplified LoadMoreButton component, added propTypes to it.

--- src/components/LoadMoreButton/LoadMoreButton.js
+++ src/components/LoadMoreButton/LoadMoreButton.js
@@ -1,19 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './LoadMoreButton.css';
 
 /**
  * Displays button used to load more movie entries.
  */
-const LoadMoreButton = props => {
-	return (
-		<div className="row">
-			<div className="load-more col-sm-10 col-sm-push-1 col-md-8 col-md-push-2" >
-				<button className="load-more--button" onClick={ props.onClick }>
-					<i className="fa fa-arrow-down load-more--icon" aria-hidden="true"></i>
-				</button>
-			</div>
+const LoadMoreButton = props => (
+	<div className="row">
+		<div className="load-more col-sm-10 col-sm-push-1 col-md-8 col-md-push-2" >
+			<button className="load-more--button" onClick={ props.onClick }>
+				<i className="fa fa-arrow-down load-more--icon" aria-hidden="true"></i>
+			</button>
 		</div>
-	);
+	</div>
+);
+
+LoadMoreButton.propTypes = {
+	onClick: PropTypes.func.isRequired
 };
 
 export default LoadMoreButton;
