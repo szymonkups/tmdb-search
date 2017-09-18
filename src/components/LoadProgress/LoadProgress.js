8d148f9661bcec38b39cf4f16c9545bf3423b318
Szymon Kupś <szymonkups@gmail.com>
2017-09-18 16:28:53 +0200

    Simplified LoadProgress component.

--- src/components/LoadProgress/LoadProgress.js
+++ src/components/LoadProgress/LoadProgress.js
@@ -4,17 +4,15 @@ import './LoadProgress.css';
 /**
  * Displays loading progress indicator.
  */
-const LoadProgress = () => {
-	return (
-		<div className="row">
-			<div className="load-progress col-sm-10 col-sm-push-1 col-md-8 col-md-push-2" >
-				<span className="load-progress--spinner">
-					<i className="fa fa-cog fa-spin fa-3x fa-fw" aria-hidden="true"></i><br />
-					Loading data...
-				</span>
-			</div>
+const LoadProgress = () => (
+	<div className="row">
+		<div className="load-progress col-sm-10 col-sm-push-1 col-md-8 col-md-push-2" >
+			<span className="load-progress--spinner">
+				<i className="fa fa-cog fa-spin fa-3x fa-fw" aria-hidden="true"></i><br />
+				Loading data...
+			</span>
 		</div>
-	);
-};
+	</div>
+);
 
 export default LoadProgress;
