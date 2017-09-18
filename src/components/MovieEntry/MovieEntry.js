32ee2dc803e1555d876360d66cedc7e3728ba6ac
Szymon Kupś <szymonkups@gmail.com>
2017-09-18 16:26:10 +0200

    Added prop-types package. Refactored MovieEntry component.

--- src/components/MovieEntry/MovieEntry.js
+++ src/components/MovieEntry/MovieEntry.js
@@ -1,28 +1,32 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import './MovieEntry.css';
 import placeholder from './placeholder.png';
+import MovieModel from '../../models/MovieModel';
 
 /**
  * Displays information about found movie.
  */
-class MovieEntry extends Component {
-	render() {
-		const movie = this.props.model;
-		const posterUrl = movie.posterUrl || placeholder;
+const MovieEntry = props => {
+	const movie = props.model;
+	const posterUrl = movie.posterUrl || placeholder;
 
-		return (
-			<li className="movie-entry col-sm-10 col-sm-push-1 col-md-8 col-md-push-2">
-				<img className="movie-entry__poster" src={ posterUrl } alt={ movie.title + ' poster' } />
-				<h2 className="movie-entry__title">{ movie.title }</h2>
-				<p className="movie-entry__tags">
-					<Tag icon="star" text={ movie.voteAverage } />&nbsp;
-					<Tag icon="calendar" text={ movie.releaseDate } />
-				</p>
-				<p className="movie-entry__overview">{ movie.overview }</p>
-			</li>
-		);
-	}
-}
+	return (
+		<li className="movie-entry col-sm-10 col-sm-push-1 col-md-8 col-md-push-2">
+			<img className="movie-entry__poster" src={ posterUrl } alt={ movie.title + ' poster' } />
+			<h2 className="movie-entry__title">{ movie.title }</h2>
+			<p className="movie-entry__tags">
+				<Tag icon="star" text={ movie.voteAverage } />&nbsp;
+				<Tag icon="calendar" text={ movie.releaseDate } />
+			</p>
+			<p className="movie-entry__overview">{ movie.overview }</p>
+		</li>
+	);
+};
+
+MovieEntry.propTypes = {
+	model: PropTypes.instanceOf( MovieModel ).isRequired
+};
 
 /**
  * Stateless component used inside MovieEntry to display single tag.
@@ -33,5 +37,10 @@ const Tag = props => (
 		&nbsp;&nbsp;
 		{ props.text }
 	</span> );
+
+Tag.propTypes = {
+	icon: PropTypes.string.isRequired,
+	text: PropTypes.string.isRequired
+};
 
 export default MovieEntry;
